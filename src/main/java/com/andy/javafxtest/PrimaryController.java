4204597922a1282388fdc04bf0e408cf571ae4a1
master <papachristos.andreas@gmail.com>
2020-11-30 14:25:46 +0200

    add treeview execution

--- src/main/java/com/andy/javafxtest/PrimaryController.java
+++ src/main/java/com/andy/javafxtest/PrimaryController.java
@@ -11,9 +11,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.ImageView;
 import javafx.stage.DirectoryChooser;
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -34,12 +36,13 @@ public class PrimaryController {
     private void getDir() throws Exception {
         //private int i = 1;
         //System.out.println(this.ckb.isSelected());
-        
+       // Node pdfico = new ImageView("img/pdf-icon-copy-min1.png");
        // tree.setExpanded(true);
         var homeDir = System.getProperty("user.home") + System.getProperty("file.separator");
         TreeItem<String> home = new TreeItem<String>(homeDir.toString());
         home.setExpanded(true);
         tree.setRoot(home);
+        home.setGraphic(new ImageView("img/folder_modernist_add.png"));
         
         DirectoryChooser dirchooser = new DirectoryChooser();
         dirchooser.setInitialDirectory(new File(homeDir));
@@ -63,7 +66,9 @@ public class PrimaryController {
                                 return p.toString();
                             })
                             .collect(Collectors.toList());
-                    pathList.forEach(String-> home.getChildren().add(new TreeItem<String>(String)));
+                    pathList.forEach(String->home.getChildren().add(new TreeItem<String>(String, (new ImageView("img/document_a4.png")))));
+                    //TreeItem.forEach(TreeItem-> TreeItem.setGraphic(new ImageView("")));
+      
                     try {
                         com.andy.helpers.PageCount.printToXml(pathList, ckb.isSelected());
 
