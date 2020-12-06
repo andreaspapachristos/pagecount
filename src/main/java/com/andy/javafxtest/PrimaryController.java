553d337e8f475554e3d4729804b1abcc9e80a5b3
master <papachristos.andreas@gmail.com>
2020-12-06 14:42:17 +0200

    add progress indicator

--- src/main/java/com/andy/javafxtest/PrimaryController.java
+++ src/main/java/com/andy/javafxtest/PrimaryController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javafx.application.Platform;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
@@ -32,6 +33,8 @@ public class PrimaryController {
     }
     @FXML
     private TextArea txt;
+    @FXML
+    private ProgressIndicator progress;
 
     @FXML
     private void txtTxt() {
@@ -48,16 +51,7 @@ public class PrimaryController {
     @FXML
     private void getDir() throws Exception {
         txtTxt();
-        //private int i = 1;
-        //System.out.println(this.ckb.isSelected());
-        // Node pdfico = new ImageView("img/pdf-icon-copy-min1.png");
-        // tree.setExpanded(true);
         var homeDir = System.getProperty("user.home") + System.getProperty("file.separator");
-        /*  TreeItem<String> home = new TreeItem<String>(homeDir.toString());
-        home.setExpanded(true);
-        tree.setRoot(home);
-        home.setGraphic(new ImageView("img/folder_modernist_add.png"));
-         */
         DirectoryChooser dirchooser = new DirectoryChooser();
         dirchooser.setInitialDirectory(new File(homeDir));
 
@@ -66,7 +60,6 @@ public class PrimaryController {
         if (ff != null) {
 
             Platform.runLater(() -> {
-                //   Runnable runnable = () -> {
                 try (Stream<Path> paths = Files.walk(Paths.get(ff.getAbsolutePath()))) {
 
                     List<String> pathList = paths
@@ -82,12 +75,9 @@ public class PrimaryController {
                                 return p.toString();
                             })
                             .collect(Collectors.toList());
-
-                    //TreeItem.forEach(TreeItem-> TreeItem.setGraphic(new ImageView("")));
-                    //  PrintStream standardOut = System.out;
                     Runnable runnable = () -> {
                         try {
-
+                            
                             com.andy.helpers.PageCount.printToXml(pathList, ckb.isSelected());
 
                         } catch (IOException ex) {
@@ -97,13 +87,7 @@ public class PrimaryController {
                         }
                     };
                     Thread thread = new Thread(runnable);
-
-                    // Thread thread = new Thread(runnable);
                     thread.start();
-
-                    /*           };
-                    Thread thread = new Thread(runnable);
-                    thread.start();*/
                     TreeItem<String> home = new TreeItem<String>(ff.getCanonicalFile().toString());
                     home.setExpanded(true);
                     tree.setRoot(home);
@@ -113,12 +97,6 @@ public class PrimaryController {
                 } catch (IOException ex) {
                     Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
                 }
-
-
-                /*  for (String f:pathList){
-                    System.out.printf("%s"+")"+"%s" +"%d"+"\n",i++, f, PageCount.efficientPDFPageCount(f));
-                  // System.out.println(PageCount.efficientPDFPageCount(f));
-                }*///paths.parallel().forEach(p-> System.out.println("Thread : " + Thread.currentThread().getName() + ", value: " + p));
             });
 
         }
