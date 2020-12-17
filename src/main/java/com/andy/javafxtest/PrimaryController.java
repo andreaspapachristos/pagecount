4c9d8236292ef3299fd11fd38366deb924ce73a3
master <papachristos.andreas@gmail.com>
2020-12-17 10:01:48 +0200

    format code

--- src/main/java/com/andy/javafxtest/PrimaryController.java
+++ src/main/java/com/andy/javafxtest/PrimaryController.java
@@ -12,17 +12,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import javafx.application.Platform;
 import javafx.concurrent.Task;
-import javafx.concurrent.WorkerStateEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
-import javafx.scene.image.ImageView;
 import javafx.stage.DirectoryChooser;
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -55,59 +49,61 @@ public class PrimaryController {
     @FXML
     private void getDir() throws Exception {
         txtTxt();
-      
+
         var homeDir = System.getProperty("user.home") + System.getProperty("file.separator");
         DirectoryChooser dirchooser = new DirectoryChooser();
         dirchooser.setInitialDirectory(new File(homeDir));
-        
- 
-        var ff = dirchooser.showDialog(null);
-        if(ff != null)
-        tree.setText(ff.getAbsolutePath().toString());
-  Task task = new Task<Void>(){
-       
-        public Void call(){
-          
-        if (ff != null) {
-             
-                try (Stream<Path> paths = Files.walk(Paths.get(ff.getAbsolutePath()))) {
-                    
-                    List<String> pathList = paths
-                            .parallel()
-                            .filter(Files::isRegularFile)
-                            .filter(path -> path.toString().endsWith(".pdf"))
-                            .filter(path -> !path.getFileName().toString().startsWith("."))
-                            //.peek(System.out::println)
-                            .map(p -> {
-                                if (Files.isDirectory(p)) {
 
-                                    return p.toString() + "/";
-                                }
-                                return p.toString();
-                            })
-                            .collect(Collectors.toList());
-                    Runnable runnable = () -> {
-                        try {                            
-                            com.andy.helpers.PageCount.printToXml(pathList, ckb.isSelected());
-                        } catch (IOException ex) {
-                            Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
-                        } catch (ParserConfigurationException ex) {
-                            Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    };
-                   
-                   new Thread(runnable).start();
-                   
-                    progress.setVisible(true);
-                } catch (IOException ex) {
-                    Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
-                    System.err.println("no directory choosed!!");
+        var ff = dirchooser.showDialog(null);
+        if (ff != null) {
+            tree.setText(ff.getAbsolutePath().toString());
+        }
+        Task task = new Task<Void>() {
+
+            public Void call() {
+
+                if (ff != null) {
+
+                    try ( Stream<Path> paths = Files.walk(Paths.get(ff.getAbsolutePath()))) {
+
+                        List<String> pathList = paths
+                                .parallel()
+                                .filter(Files::isRegularFile)
+                                .filter(path -> path.toString().endsWith(".pdf"))
+                                .filter(path -> !path.getFileName().toString().startsWith("."))
+                                //.peek(System.out::println)
+                                .map(p -> {
+                                    if (Files.isDirectory(p)) {
+
+                                        return p.toString() + "/";
+                                    }
+                                    return p.toString();
+                                })
+                                .collect(Collectors.toList());
+                        Runnable runnable = () -> {
+                            try {
+                                com.andy.helpers.PageCount.printToXml(pathList, ckb.isSelected());
+                            } catch (IOException ex) {
+                                Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
+                            } catch (ParserConfigurationException ex) {
+                                Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        };
+
+                        new Thread(runnable).start();
+
+                        progress.setVisible(true);
+                    } catch (IOException ex) {
+                        Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
+                        System.err.println("no directory choosed!!");
+                    }
+
                 }
-                            
-        }return null;
-        
-        };
-        
+                return null;
+
+            }
+        ;
+
         };
         progress.visibleProperty().bind(task.runningProperty());
         new Thread(task).start();
